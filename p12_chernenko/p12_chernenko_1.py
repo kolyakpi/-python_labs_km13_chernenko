f60ad11005e3811c1970260f3653c5078a66cae2
kolyakpi <91086565+kolyakpi@users.noreply.github.com>
2021-11-22 18:31:46 +0200

    Update p12_chernenko_1.py

--- p12_chernenko/p12_chernenko_1.py
+++ p12_chernenko/p12_chernenko_1.py
@@ -21,50 +21,55 @@ dirs = [
 ]
  
 # ВАШ КОД ТУТ
- 
-def search(dirs, filename, string="", arr=[]): 
+
+def flat(S):
+    if S == []:
+        return S
+    if type(S[0]) == list:
+        return flat(S[0]) + flat(S[1:])
+    return S[:1] + flat(S[1:])
+
+def search(dirs, filename, string="", fold = ""): 
+    arr = []
     if filename == dirs[0][0]:
         return []
-    
     for i in range(len(dirs)):
-        
         repos = string
-        flag = 1
         if type(dirs[i]) == tuple:
             string += "/" + dirs[i][0]
-            search(dirs[i], filename, string, arr)
+            arr.append(search(dirs[i], filename, string, fold=dirs[i][0]))
         elif type(dirs[i]) == list:
-            search(dirs[i], filename, string, arr)
+            arr.append(search(dirs[i], filename, string))
         elif dirs[i] == filename:
-            arr.append(string+"/"+dirs[i])
-            flag = 0
+            if filename != fold:
+                arr.append(string+"/"+dirs[i])
+            else:
+                arr.append(string)
         string = repos
-        
-    return arr
+    return flat(arr)
 
 
 # ПЕРЕВІРКА
- 
+
+print(search(dirs, 'folder1'))
+print(search(dirs, 'folder2'))
+print(search(dirs, 'folder4'))
 print(search(dirs, 'file1'))
-print(search(dirs, 'file2', arr=[]))
-print(search(dirs, 'file3', arr=[]))
-print(search(dirs, 'file4', arr=[]))
-print(search(dirs, 'file5', arr=[]))
-print(search(dirs, 'file6', arr=[]))
-print(search(dirs, 'folder1', arr=[]))
+print(search(dirs, 'file2'))
+print(search(dirs, 'file3'))
+print(search(dirs, 'file4'))
+print(search(dirs, 'file5'))
+print(search(dirs, 'file6'))
+print(search(dirs, 'folder1'))
  
-assert search(dirs, 'file1', arr=[]) == ['/folder1/file1'], 'Failed test for file1'
-assert search(dirs, 'file2', arr=[]) == ['/folder1/folder2/file2'], 'Failed test for file2'
-assert search(dirs, 'file3', arr=[]) == ['/folder1/folder2/file3', '/folder1/folder3/file3', '/folder1/folder3/folder4/file3'], 'Failed test for file3'
-assert search(dirs, 'file4', arr=[]) == ['/folder1/folder3/file4'], 'Failed test for file4'
-assert search(dirs, 'file5', arr=[]) == ['/folder1/file5'], 'Failed test for file5'
-assert search(dirs, 'file6', arr=[]) == [], 'Failed test for file6'
-assert search(dirs, 'folder1', arr=[]) == [], 'Failed test for folder1'
+assert search(dirs, 'file1') == ['/folder1/file1'], 'Failed test for file1'
+assert search(dirs, 'file2') == ['/folder1/folder2/file2'], 'Failed test for file2'
+assert search(dirs, 'file3') == ['/folder1/folder2/file3', '/folder1/folder3/file3', '/folder1/folder3/folder4/file3'], 'Failed test for file3'
+assert search(dirs, 'file4') == ['/folder1/folder3/file4'], 'Failed test for file4'
+assert search(dirs, 'file5') == ['/folder1/file5'], 'Failed test for file5'
+assert search(dirs, 'file6') == [], 'Failed test for file6'
+assert search(dirs, 'folder1') == [], 'Failed test for folder1'
 print('All tests good!')
-
-
-
-
 
 
 
